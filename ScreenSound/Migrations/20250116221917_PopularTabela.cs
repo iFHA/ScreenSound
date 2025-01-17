0dc9ad98d6224ff6c9db1cf059b74dcfbdcacd6a
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2025-01-17 18:03:52 -0300

    add migrations

---
+++ ScreenSound/Migrations/20250116221917_PopularTabela.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabela : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Artistas", new string[] {"Nome", "Bio", "FotoPerfil"}, new object[] {
+                "teste Djavan", "bio djavan", "foto_djavan.png"
+            });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Artistas");
+        }
+    }
+}

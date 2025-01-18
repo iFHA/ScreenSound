522a9f06f0bcee00ce9bcc7fb5c8669e31f03975
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2025-01-18 12:19:13 -0300

    Add metodos GET e POST Artista

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,16 +1,42 @@
+using System.Net;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 var app = builder.Build();
 
-app.MapGet("/", () =>
+app.MapGet("/Artistas", () =>
 {
     var context = new ScreenSoundContext();
     var dal = new DAL<Artista>(context);
-    return dal.Listar();
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Artistas/{nome}", (string nome) =>
+{
+    var context = new ScreenSoundContext();
+    var dal = new DAL<Artista>(context);
+
+    var result = dal.RecuperarPor(artista => artista.Nome.ToLower().Contains(nome.ToLower()));
+    if (result is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(result);
+
+});
+
+app.MapPost("/Artistas", ([FromBody] Artista artista) =>
+{
+    var context = new ScreenSoundContext();
+    var dal = new DAL<Artista>(context);
+    dal.Adicionar(artista);
+    return Results.Created();
 });
 
 app.Run();

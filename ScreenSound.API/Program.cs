12f91bf5b97a4d136a741c4f577adc3b95454efd
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2025-01-18 13:40:50 -0300

    add injeção de dependencias

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -7,20 +7,18 @@ using ScreenSound.Modelos;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
+
 var app = builder.Build();
 
-app.MapGet("/Artistas", () =>
+app.MapGet("/Artistas", (DAL<Artista> dal) =>
 {
-    var context = new ScreenSoundContext();
-    var dal = new DAL<Artista>(context);
     return Results.Ok(dal.Listar());
 });
 
-app.MapGet("/Artistas/{nome}", (string nome) =>
+app.MapGet("/Artistas/{nome}", (string nome, DAL<Artista> dal) =>
 {
-    var context = new ScreenSoundContext();
-    var dal = new DAL<Artista>(context);
-
     var result = dal.RecuperarPor(artista => artista.Nome.ToLower().Contains(nome.ToLower()));
     if (result is null)
     {
@@ -31,10 +29,8 @@ app.MapGet("/Artistas/{nome}", (string nome) =>
 
 });
 
-app.MapPost("/Artistas", ([FromBody] Artista artista) =>
+app.MapPost("/Artistas", ([FromBody] Artista artista, DAL<Artista> dal) =>
 {
-    var context = new ScreenSoundContext();
-    var dal = new DAL<Artista>(context);
     dal.Adicionar(artista);
     return Results.Created();
 });

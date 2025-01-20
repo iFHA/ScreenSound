0aeda5a2e880b75d87185cb2dbd1fc53b97f40a5
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2025-01-20 20:18:57 -0300

    Add migration de genero

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -4,16 +4,17 @@ using ScreenSound.Modelos;
 
 namespace ScreenSound.Banco
 {
-    public class ScreenSoundContext: DbContext
+    public class ScreenSoundContext : DbContext
     {
         public DbSet<Artista> Artistas { get; set; }
         public DbSet<Musica> Musicas { get; set; }
+        public DbSet<Genero> Generos { get; set; }
         private string ConnectionString = "Data Source=localhost;Initial Catalog=ScreenSound;User ID=sa;Password=Dbzbt333@;TrustServerCertificate=True";
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
             .UseSqlServer(ConnectionString)
             .UseLazyLoadingProxies();
-        }        
+        }
     }
 }
